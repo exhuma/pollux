7b8e54e6eb47f7a8c47666bc6782213261b26925
Michel Albert <michel@albert.lu>
2019-08-12 07:35:15 +0200

    WIP

--- pollux/emitter.py
+++ pollux/emitter.py
@@ -3,6 +3,7 @@ Emitters are responsible to work with both warningins and raw pollen data.
 Emitters can have one or more handlers which /do/ something with the data they
 are given.
 '''
+from warnings import warn
 import logging
 
 LOG = logging.getLogger(__name__)
@@ -25,6 +26,23 @@ class MemoryHandler:
         self.disseminated_data = data
 
 
+class CSVHandler:
+    """
+    Write the raw data to disk as CSV values.
+
+    warnings are ignored.
+    """
+
+    def __init__(self, root_folder=None):
+        self.root_folder = root_folder or 'csv_data'
+
+    def handle(self, pollen_family, symptom_strength):
+        warn('Not yet implemented', NotImplemented)
+
+    def handle_raw_data(self, data):
+        print(data)  # XXX debug statement
+
+
 class Emitter:
 
     def __init__(self):
@@ -44,3 +62,14 @@ class Emitter:
         }
         for handler in self.handlers:
             handler.handle_raw_data(output)
+
+
+class CSVEmitter:
+
+    def __init__(self):
+            self.handlers = set()
+
+    def disseminate(self, date, values):
+        print(date, len(values))  # XXX debug statement
+        from pprint import pprint; pprint(date)  # XXX debug statement
+        from pprint import pprint; pprint(values)  # XXX debug statement
