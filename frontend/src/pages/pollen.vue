9c52204c47919490d5bc18494d53e7b7749c789a
Michel Albert <michel@albert.lu>
2020-08-16 13:21:51 +0200

    Reduce code-duplication

--- frontend/src/pages/pollen.vue
+++ frontend/src/pages/pollen.vue
@@ -11,32 +11,35 @@ export default {
   data () {
     return {
       model: null,
-      options: ['Acer', 'Gramineae']
+      options: ['Acer', 'Gramineae'],
+      graphLayout: {
+        barmode: 'stack',
+        yaxis: { fixedrange: true }
+      }
     }
   },
   watch: {
     model: function (newValue, oldValue) {
       let url = `http://localhost:5000/recent?num_days=200&genus=${newValue}`
+      let that = this
       this.$axios.get(url).then(response => {
         let values = response.data[newValue]
         let data = [values]
-        let layout = { barmode: 'stack' }
-        Plotly.react('Graph', data, layout)
+        Plotly.react('Graph', data, that.graphLayout)
       })
     }
   },
   mounted () {
     var data = []
-    var layout = { barmode: 'stack' }
-    Plotly.newPlot('Graph', data, layout)
+    Plotly.newPlot('Graph', data, this.graphLayout)
   },
   created () {
     let url = 'http://localhost:5000/recent?num_days=200&genus=Acer'
+    let that = this
     this.$axios.get(url).then(response => {
       let acer = response.data['Acer']
       let data = [acer]
-      let layout = { barmode: 'stack' }
-      Plotly.react('Graph', data, layout)
+      Plotly.react('Graph', data, that.graphLayout)
     })
   }
 }
