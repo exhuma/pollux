34cfa3fbb9ae1ebb93f3e3a812fb2dd89a721b81
Michel Albert <michel@albert.lu>
2022-06-15 20:32:09 +0000

    WIP: Prepare migration away from quasar
    Moving UI into separate "pollux-frontend" project

--- frontend/src/layouts/main.vue
+++ frontend/src/layouts/main.vue
@@ -4,24 +4,6 @@
     <q-header reveal elevated class="bg-primary text-white">
       <q-toolbar>
         <q-btn dense flat round icon="menu" @click="leftDrawer = !leftDrawer" />
-
-        <q-toolbar-title>
-          Pollen/Spores - Luxembourg
-        </q-toolbar-title>
-
-        <q-select
-          v-model="lang"
-          :options="langOptions"
-          label="Language"
-          dense
-          borderless
-          emit-value
-          map-options
-          options-dense
-          style="min-width: 150px"
-          />
-        <q-btn v-if="token === ''" rounded flat @click="startLogin" icon="login" />
-        <q-btn v-if="token !== ''" rounded flat @click="logout" icon="account_circle" />
       </q-toolbar>
     </q-header>
 
@@ -31,49 +13,6 @@
       bordered
       content-class="bg-grey-2"
     >
-      <q-list>
-        <q-item-label header>Essential Links</q-item-label>
-
-        <q-item to="/pollen" exact>
-          <q-item-section avatar>
-            <q-icon name="gps_fixed" />
-          </q-item-section>
-          <q-item-section>
-            <q-item-label>Pollen</q-item-label>
-            <q-item-label caption>Pollen concentration</q-item-label>
-          </q-item-section>
-        </q-item>
-
-        <q-item to="/spores" exact>
-          <q-item-section avatar>
-            <q-icon name="bug_report" />
-          </q-item-section>
-          <q-item-section>
-            <q-item-label>Spores</q-item-label>
-            <q-item-label caption>Spore Concentration</q-item-label>
-          </q-item-section>
-        </q-item>
-
-        <q-item to="/information" exact>
-          <q-item-section avatar>
-            <q-icon name="school" />
-          </q-item-section>
-          <q-item-section>
-            <q-item-label>Information</q-item-label>
-            <q-item-label caption>Information about spores &amp; poolen</q-item-label>
-          </q-item-section>
-        </q-item>
-
-        <q-item to="/data" exact>
-          <q-item-section avatar>
-            <q-icon name="archive" />
-          </q-item-section>
-          <q-item-section>
-            <q-item-label>Data Management</q-item-label>
-            <q-item-label caption>Add new data to the backend</q-item-label>
-          </q-item-section>
-        </q-item>
-      </q-list>
     </q-drawer>
 
     <q-page-container>
@@ -120,89 +59,14 @@
 </template>
 
 <script>
-import { Proxy } from 'src/proxy.js'
-
-function getLanguage (proxy) {
-  return proxy.getSupportedLanguages()
-    .then(supportedLanguages => {
-      let browserSetting = navigator.languages || [navigator.language]
-      let bestMatch = ''
-      browserSetting.forEach(item => {
-        let langCode = item.split('-')[0]
-        if (supportedLanguages.indexOf(langCode) > -1 && bestMatch === '') {
-          bestMatch = langCode
-        }
-      })
-      return bestMatch
-    })
-}
 
 export default {
   data () {
     return {
       leftDrawer: false,
-      token: '',
-      proxy: null,
-      lang: this.$i18n.locale,
-      loginDialog: false,
       username: '',
-      password: '',
       isPwd: true,
-      langOptions: [
-        { value: 'en', label: 'English' },
-        { value: 'de', label: 'Deutsch' },
-        { value: 'fr', label: 'Français' },
-        { value: 'lb', label: 'Lëtzebuergesch' }
-      ]
     }
   },
-  methods: {
-    startLogin () {
-      this.loginDialog = true
-    },
-    logout () {
-      this.login = ''
-      this.password = ''
-      this.onTokenChanged('')
-    },
-    acceptLogin () {
-      if (this.username && this.password) {
-        this.proxy.login(this.username, this.password)
-          .then(token => {
-            this.onTokenChanged(token)
-          })
-      }
-      this.password = ''
-      this.loginDialog = false
-    },
-    cancelLogin () {
-      this.login = ''
-      this.password = ''
-      this.loginDialog = false
-    },
-    onTokenChanged (token) {
-      if (!token) {
-        localStorage.removeItem('token')
-        this.token = ''
-        this.proxy.setToken('')
-        return
-      }
-      localStorage.setItem('token', token)
-      this.token = token
-      this.proxy.setToken(token)
-    }
-  },
-  watch: {
-    lang (lang) {
-      this.$i18n.locale = lang
-    }
-  },
-  created () {
-    this.token = localStorage.getItem('token') || ''
-    this.proxy = new Proxy('http://localhost:5000', this.token)
-    getLanguage(this.proxy).then(value => {
-      this.lang = value
-    })
-  }
 }
 </script>
